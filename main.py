6a4881fc90f49fd688fec047ce39c7baf0eb3f42
raviheima <vctroreo@gmail.com>
2025-09-24 16:24:30 +0100

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 name, name_length = "neut", 4
+list = [1,2,3,4]
+dict = {"ls":"list current directory"}
+tuple = ("food", "stew")
+set = {1,2,3,4}
+frozenset = { ("am", "i", "frozen")}
 
+var_list = [list,dict,tuple,set,frozenset]
 print(name)
 print(name_length)
 
+print(type(name_length))
+
+for value in var_list:
+    print(type(value))
+
+boy = "7"
+
+hmm = int(boy)
+print("i am an ", type(hmm))
