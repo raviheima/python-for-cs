79cdf9a7fc64b19633f17416ca677fb6dd13ba7f
raviheima <vctroreo@gmail.com>
2025-09-25 16:31:55 +0100

    done with problem 5

---
+++ practice5.py
@@ -0,0 +1,7 @@
+# Write a program that asks the user to enter a weight in kilograms. The program should
+# convert it to pounds, printing the answer rounded to the nearest tenth of a pound.
+
+weight_in_kg = float(input("Enter your weight in KG:"))
+KGToPounds = weight_in_kg * 2.2 
+rounded_pounds = round(KGToPounds,1)
+print(f"{rounded_pounds}")
