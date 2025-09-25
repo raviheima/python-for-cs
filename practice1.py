a580aaa52f44d22cb9441c8430499f2c54e0417a
raviheima <vctroreo@gmail.com>
2025-09-25 15:27:56 +0100

    solved practice question 1

---
+++ practice1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Kilograms to pounds converter!")
+
+
+weight = input("Enter your weight in kilograms: ")
+pounds = float(weight) * 2.2
+
+print("your weight is ", pounds, "pounds")
